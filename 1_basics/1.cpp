3f07c41dc8c83cc88fb314005458fde1ac7c5845
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2026-03-03 23:47:28 +0530

    Final check commit

--- 1_basics/1.cpp
+++ 1_basics/1.cpp
@@ -29,11 +29,11 @@ int main()
 {
     Employee obj1;
     obj1.setName("Abhinav");
-    obj1.setSalary(100000);
+    obj1.setSalary(1000000);
 
     Employee obj2;
     obj2.setName("ZiKt0R");
-    obj2.setSalary(100000);
+    obj2.setSalary(1000000);
 
     cout << "Salary of " << obj1.employeeName << " is " << obj1.getSalary() << endl;
     cout << "Salary of " << obj2.employeeName << " is " << obj2.getSalary() << endl;
